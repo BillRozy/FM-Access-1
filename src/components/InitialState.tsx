f3054dfd9fda82ebae87e3883a2bc1d8292e19a3
BillRozy <squee1994@gmail.com>
2024-06-11 18:46:32 +0200

    fix axe report

--- src/components/InitialState.tsx
+++ src/components/InitialState.tsx
@@ -20,9 +20,7 @@ export default function InitialState({
       </p>
       <ul role="radiogroup" aria-required aria-label="List of buttons to selected desired rating value" className="rating-list">
         {ratingItems.map((item) => (
-          <li key={item}>
-            <button aria-label={`Rating ${item}`} className="rating-list-item" aria-selected={item === tempRating} onClick={() => setTempRating(item)}>{item}</button>
-          </li>
+            <button key={item} role="radio" aria-label={`Rating ${item}`} className="rating-list-item" aria-checked={item === tempRating} onClick={() => setTempRating(item)}>{item}</button>
         ))}
       </ul>
       <button aria-label={tempRating !== -1 ? `Submit selected rating ${tempRating}` : "You should select rating first before clicking this button"} className="button button--primary" onClick={() => onRatingSelected(tempRating)}>Submit</button>
--- src/components/RatingSetState.tsx
+++ src/components/RatingSetState.tsx
@@ -1,10 +1,11 @@
 import "../assets/styles/RatingSetState.css"
+import imgSvg from  "../assets/images/illustration-thank-you.svg"
 export default function RatingSetState({ rating }: { rating: number }) {
   return (
     <div className="flex-block rating-set-block">
       <img
         className="img--transaction"
-        src="/illustration-thank-you.svg"
+        src={imgSvg}
         alt="There is a credit card and bill printed by electronic device described on a picture, symbolizing successful transaction"
       ></img>
       <span className="tag tag--primary">You selected {rating} out of 5</span>
